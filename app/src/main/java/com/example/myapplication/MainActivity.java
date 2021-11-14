8b172ec653f142313552c80369c28661e6cc746e
lennwouh <leyuma256@gmail.com>
2021-11-14 14:23:40 +0100

    Storing and retrieving data via SharedPreferences

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView text2;           //declare variable in class => can be used in different functions other than onCreate()
     public int clickerScore = 0;
+    public SharedPreferences sharedPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,10 +58,11 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         System.out.println("onPause");
         //create SharedPreferences => data stored and saved even in case of onDestroy()
-        SharedPreferences sharedPreferences = getSharedPreferences("mSharedPreferences", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();             //edit SharedPreference
+        sharedPreferences = getSharedPreferences("mSharedPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();             //edit SharedPreferences
         editor.putInt("CLICKER_SCORE", Integer.parseInt(text2.getText().toString()));              //add values (key-paired) to be saved
-        editor.apply();     //apply changes                                                       // see: https://developer.android.com/reference/android/content/SharedPreferences
+        editor.apply();     //apply changes                                                        // see: https://developer.android.com/reference/android/content/SharedPreferences
+        System.out.println("[DEBUG] Data saved");
     }
 
     public void onStart(){
@@ -72,6 +74,12 @@ public class MainActivity extends AppCompatActivity {
     public void onResume(){
         super.onResume();
         System.out.println("onResume");
+        if(sharedPreferences == null) {
+            SharedPreferences sP = getSharedPreferences("mSharedPreferences", MODE_PRIVATE);
+            int CLICKER = sP.getInt("CLICKER_SCORE", 0);                                 //retrieve data from SharedPreferences
+            text2.setText("" + CLICKER);                                    //update value of TextView with stored data
+            clickerScore = CLICKER;                     //adapting clickerScore to value of CLICKER when setOnClickListener
+        }
     }
 
     public void onDestroy(){
